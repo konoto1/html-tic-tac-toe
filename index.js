2226c73636ae63e0401f27c24edf5443de7f2259
Tomas.K <tomas.konopackis@gmail.com>
2024-08-05 18:38:54 +0300

    updated with initial score count

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ class Game {
     constructor() {
         this.buttonDOM = document.querySelectorAll('.game > button');
         this.playAgainDOM = document.querySelector('.playAgain');
+        this.resultDOM = document.querySelector('.result');
+        this.scoreODOM = document.getElementById('scoreO');
+        this.scoreXDOM = document.getElementById('scoreX');
 
         this.isGameDataNotEmpty();
         this.beginGame();
         this.gameEvents();
-
+        this.winCheck();
     }
 
     isGameDataNotEmpty() {
@@ -44,6 +47,7 @@ class Game {
 
 
         for (let i = 0; i < this.buttonDOM.length; i++) {
+
             this.buttonDOM[i].addEventListener('click', () => {
                 const isEmpty = this.buttonDOM[i].textContent.length === 0;
                 if (isEmpty && count === 0) {
@@ -55,6 +59,7 @@ class Game {
                     });
                     count = 1;
                     localStorage.setItem('gameInfo', JSON.stringify(gameData));
+                    this.winCheck();
 
                 } else if (isEmpty && count !== 0) {
                     this.buttonDOM[i].textContent = 'X';
@@ -66,15 +71,55 @@ class Game {
                     });
                     count = 0;
                     localStorage.setItem('gameInfo', JSON.stringify(gameData));
+                    this.winCheck();
                 }
             });
-        }
 
+
+        }
         this.playAgainDOM.addEventListener('click', () => {
             gameData = [];
-            localStorage.setItem('gameInfo', JSON.stringify(gameData))
+            localStorage.setItem('gameInfo', JSON.stringify(gameData));
+            this.resultDOM.textContent = '';
             this.beginGame();
         });
+    }
+
+    winCheck() {
+        let btn = []
+        let scoreO = 0;
+        let scoreX = 0;
+        for (let i = 0; i < this.buttonDOM.length; i++) {
+            btn.push(this.buttonDOM[i].textContent);
+        }
+        if (btn[0] + btn[1] + btn[2] === 'OOO'
+            || btn[0] + btn[1] + btn[2] === 'OOO'
+            || btn[3] + btn[4] + btn[5] === 'OOO'
+            || btn[6] + btn[7] + btn[8] === 'OOO'
+            || btn[0] + btn[3] + btn[6] === 'OOO'
+            || btn[1] + btn[4] + btn[7] === 'OOO'
+            || btn[2] + btn[5] + btn[8] === 'OOO'
+            || btn[0] + btn[4] + btn[8] === 'OOO'
+            || btn[2] + btn[4] + btn[6] === 'OOO') {
+            this.resultDOM.textContent = 'Player "O" won!';
+            scoreO++;
+            this.scoreODOM.textContent = scoreO;
+            this.beginGame();
+        }
+        if (btn[0] + btn[1] + btn[2] === 'XXX'
+            || btn[0] + btn[1] + btn[2] === 'XXX'
+            || btn[3] + btn[4] + btn[5] === 'XXX'
+            || btn[6] + btn[7] + btn[8] === 'XXX'
+            || btn[0] + btn[3] + btn[6] === 'XXX'
+            || btn[1] + btn[4] + btn[7] === 'XXX'
+            || btn[2] + btn[5] + btn[8] === 'XXX'
+            || btn[0] + btn[4] + btn[8] === 'XXX'
+            || btn[2] + btn[4] + btn[6] === 'XXX') {
+            this.resultDOM.textContent = 'Player "X" won!';
+            scoreX++;
+            this.scoreXDOM.textContent = scoreX;
+            this.beginGame();
+        }
     }
 }
 
